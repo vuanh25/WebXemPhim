cbe701f5e6fa688be42aa7c03ae1e758f6f6dfba
VuCoder <chimcudammua@gmail.com>
2023-05-18 23:23:29 +0700

    Push all

---
+++ src/main/java/com/example/webxemphim/WebXemPhimApplication.java
@@ -0,0 +1,13 @@
+package com.example.webxemphim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebXemPhimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebXemPhimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webxemphim/WebXemPhimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webxemphim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebXemPhimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
